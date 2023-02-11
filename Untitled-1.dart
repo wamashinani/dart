e1efd693620ac84edb1ff5f7ee4c36368bcaf72d
Erick siele <111025605+wamashinani@users.noreply.github.com>
2023-02-11 23:00:51 +0300

    Add files via upload
    this is my first code using the new dart language 
    it is used to implement if else statement

---
+++ Untitled-1.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+  
+    void main(''); {
+    print('Enter a number.');
+    
+
+    number = int.parse(stdin.readlinesync('')!);
+
+    if('number > 10'); {
+    print('your number is greater than 10');
+    }
+    else if('number < 10'); {
+    print('your number is less than 10');
+    }
+    else if('number == 10'); {
+    print('your number is equal to 10');
+    }
+}
+
